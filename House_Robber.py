336968d53de68d7440292574f382dec03245fc4b
Khaled Awad <khaledbnmohamed@hotmail.com>
2020-04-29 03:28:11 +0200

    Update House_Robber.py

--- House_Robber.py
+++ House_Robber.py
@@ -1,5 +1,10 @@
 # https://leetcode.com/problems/house-robber/submissions/
-#top down recursive with a memory to prevent repeating myself trial #2
+# Based on solution :  https://leetcode.com/problems/house-robber/discuss/156523/From-good-to-great.-How-to-approach-most-of-DP-problems.
+# Steps :
+# 1) Think about recursive relation
+# 2) try to apply it 
+# 3) optimize it by adding a memory to save repeated values
+# top down recursive with a memory to prevent repeating myself trial #2
 # Runtime: 24 ms, faster than 92.54% of Python3 online submissions for House Robber.
 # Memory Usage: 13.8 MB, less than 9.09% of Python3 online submissions for House Robber.
 
