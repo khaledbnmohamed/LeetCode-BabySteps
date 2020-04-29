76c76d9afe10ae40d3e0d657252110c52e9f2165
Khaled Awad <khaledbnmohamed@hotmail.com>
2020-04-29 04:47:25 +0200

    adding template dp

--- House_Robber.py
+++ House_Robber.py
@@ -4,7 +4,11 @@
 # 1) Think about recursive relation
 # 2) try to apply it 
 # 3) optimize it by adding a memory to save repeated values
-# top down recursive with a memory to prevent repeating myself trial #2
+
+############################################
+# trial # 2
+# top down recursive with a memory to prevent repeating myself 
+############################################
 # Runtime: 24 ms, faster than 92.54% of Python3 online submissions for House Robber.
 # Memory Usage: 13.8 MB, less than 9.09% of Python3 online submissions for House Robber.
 
@@ -22,3 +26,24 @@ class Solution:
         result = max(self.recuRob(nums,size-2,memo)+nums[size],self.recuRob(nums,size-1,memo))
         memo[size] = result
         return result
+
+############################################
+# trial # 3
+# itertaive DP following the dp template
+############################################
+# Runtime: 28 ms, faster than 74.79% of Python3 online submissions for House Robber.
+# Memory Usage: 13.8 MB, less than 9.09% of Python3 online submissions for House Robber.
+
+class Solution:
+    
+    def rob(self, nums: List[int]) -> int:
+        size = len(nums)
+
+        if size==0: return 0
+        memo = [0]
+        memo.append(nums[0])
+            
+        for i in range(1,size):
+            memo.append(max(memo[i],memo[i-1]+nums[i]))
+            
+        return(memo[size])
