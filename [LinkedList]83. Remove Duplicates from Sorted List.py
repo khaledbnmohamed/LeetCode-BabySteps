82848c83f4a7771b3724671f9f3b19b626ff2ff2
Khaled Awad <khaledbnmohamed@hotmail.com>
2020-05-30 13:28:08 +0200

    Update [LinkedList]83. Remove Duplicates from Sorted List.py

--- [LinkedList]83. Remove Duplicates from Sorted List.py
+++ [LinkedList]83. Remove Duplicates from Sorted List.py
@@ -20,3 +20,29 @@ class Solution:
             if head.next:
                 head = head.next
         return head1
+####################################################
+# #2 trial 
+
+# Runtime: 44 ms, faster than 53.94% of Python3 online submissions for Remove Duplicates from Sorted List.
+# Memory Usage: 13.9 MB, less than 6.45% of Python3 online submissions for Remove Duplicates from Sorted List.
+    
+    
+    # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        head1 = head
+        try:
+            while head.next:
+                value = head.val
+                while head.next.val == value:
+                    head.next= head.next.next
+
+                if head.next:
+                    head = head.next
+        except:
+            return head1
+        return head1        
