bc8dec56e7015e02252926a8ea39979ae48280f5
Khaled Awad <khaledbnmohamed@hotmail.com>
2020-08-01 02:37:10 +0200

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,2 +1,19 @@
 #https://leetcode.com/problems/two-sum/
 
+#Runtime: 1412 ms, faster than 21.24% of Python3 online submissions for Two Sum.
+#Memory Usage: 14.9 MB, less than 74.32% of Python3 online submissions for Two Sum.
+#
+
+# 1st attp
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            stored_val = nums[i]
+            other_value = target- stored_val
+            nums[i] = "#"
+            try:
+                y = nums.index(other_value)
+                return [y,i]
+            except:
+                nums[i] = stored_val
+
