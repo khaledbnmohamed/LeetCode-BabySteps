1fa1ac6429116408db40db07bd67fb846a563de1
Khaled Awad <khaledbnmohamed@hotmail.com>
2020-09-01 00:19:52 +0200

    Update 237. Delete Node in a Linked List.py

--- 237. Delete Node in a Linked List.py
+++ 237. Delete Node in a Linked List.py
@@ -22,3 +22,23 @@ class Solution:
             node = node.next
         node.val = node.next.val
         node.next = None
+
+        
+#2nd attempt  , onlu change the node needed
+# Runtime: 52 ms, faster than 33.85% of Python3 online submissions for Delete Node in a Linked List.
+# Memory Usage: 14.1 MB, less than 78.55% of Python3 online submissions for Delete Node in a Linked List.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
