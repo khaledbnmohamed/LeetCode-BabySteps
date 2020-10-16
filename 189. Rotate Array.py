9c70a5c0e6f379a49448a7a854efc6e16b391689
Khaled Awad <khaledbnmohamed@hotmail.com>
2020-10-16 22:01:54 +0200

    Update 189. Rotate Array.py

--- 189. Rotate Array.py
+++ 189. Rotate Array.py
@@ -1,5 +1,8 @@
 #https://leetcode.com/problems/rotate-array/
 
+# Runtime: 76 ms, faster than 37.10% of Python3 online submissions for Rotate Array.
+# Memory Usage: 15.4 MB, less than 7.30% of Python3 online submissions for Rotate Array.
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
