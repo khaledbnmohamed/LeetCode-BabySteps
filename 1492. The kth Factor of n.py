64080bebfdb2b7f6c78fded4c64d0955a5593efc
Khaled Awad <khaledbnmohamed@hotmail.com>
2020-12-05 01:52:28 +0200

    Update 1492. The kth Factor of n.py

--- 1492. The kth Factor of n.py
+++ 1492. The kth Factor of n.py
@@ -1,5 +1,8 @@
-https://leetcode.com/problems/the-kth-factor-of-n/
-
+#https://leetcode.com/problems/the-kth-factor-of-n/
+#Runtime: 36 ms, faster than 25.30% of Python3 online submissions for The kth Factor of n.
+#Memory Usage: 14.1 MB, less than 35.66% of Python3 online submissions for The kth Factor of n.
+    
+    
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
         factors = [1]
