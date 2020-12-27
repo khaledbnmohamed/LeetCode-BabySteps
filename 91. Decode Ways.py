e61c6df8f8da82cd350a2e91ddc8f0abcacd38a2
Khaled Awad <khaledbnmohamed@hotmail.com>
2020-12-27 02:19:50 +0200

    Update 91. Decode Ways.py

--- 91. Decode Ways.py
+++ 91. Decode Ways.py
@@ -1,5 +1,27 @@
 #https://leetcode.com/problems/decode-ways/
 
+#my personal updated solution :
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if len(s) == 0 or s[0] == '0': return 0
+        if len(s) == 1: return 1
+        size = len(s)
+        arr= [1] * (size+1)
+        for i in range(2,size+1):
+            if s[i-1] == '0':
+                if (s[i-2] != '1' and s[i-2] != '2'): return 0
+                else:
+                    arr[i] = arr[i-2]
+            else:
+                number = int(s[i-2]+s[i-1])
+                if 10 <= number <= 26:
+                    arr[i] = arr[i-1] +arr[i-2]
+                else:
+                    arr[i] = arr[i-1]
+        return arr[-1]
+    
+
 
 
 class Solution:
